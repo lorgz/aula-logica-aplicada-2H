df3a82a002da89b4863d4a8c9c9bcb979df87801
lorg_z <lujdruv9x@gmail.com>
2023-04-13 08:17:34 -0300

    Criado Laço While e For.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int contador = 0; //Declarar variavel e dando valor de 0 e aceita somnte numeros inteiros
+
+        while(contador <10){ //enquanto contador menor que 10 executa a linha 6
+            System.out.println("Contador =" + contador);
+            //incremento, mudar o valor do contador
+            contador = contador + 1;
+        }
+        //laço para faça
+
+        for(int x=0; x<10; x++){
+            System.out.println("Contador =" + x);
+        }
     }
 }
