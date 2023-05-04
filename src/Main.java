0408d74ded001d8fbc9c21fd3f9a5986323a7f6f
lorg_z <lujdruv9x@gmail.com>
2023-05-04 09:54:28 -0300

    Aula de introdução OO (*Orientação e Objetos*)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int contador = 0; //Declarar variavel e dando valor de 0 e aceita somnte numeros inteiros
+        // Aula Introdutória de Orientação á Objetos
 
-        while(contador <10){ //enquanto contador menor que 10 executa a linha 6
-            System.out.println("Contador =" + contador);
-            //incremento, mudar o valor do contador
-            contador = contador + 1;
-        }
-        //laço para faça
 
-        for(int x=0; x<10; x++){
-            System.out.println("Contador =" + x);
-        }
     }
+
 }
