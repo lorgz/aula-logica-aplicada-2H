d1be869a4ac06e0a91a6f3b8446c8bcf08fe9939
lorg_z <lujdruv9x@gmail.com>
2023-05-18 11:04:09 -0300

    Aula de introdução OO (*Orientação e Objetos*) Diferença de Void e String para metodos da classe

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@ public class Main {
     public static void main(String[] args) {
         // Aula Introdutória de Orientação á Objetos
 
+        // Declaração de objeto
+        Pessoa adao;
+
+        // Instanciação do objeto
+        adao = new Pessoa();
+
+        // Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+
+        //Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
+
 
     }
 
---
+++ src/Pessoa.java
@@ -0,0 +1,16 @@
+public class Pessoa {
+
+    // Atributos da Clase
+    public String nome;
+    public String sobrenome;
+
+    //Métodos da classe
+    public void falar(){ //Void é um método sem retorno
+        System.out.println("falei");
+    }
+    public String falar(String texto){ //String é um método com retorno
+        return texto;
+    }
+
+
+}
